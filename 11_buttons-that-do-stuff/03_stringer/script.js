0b913de0b4a28f8be7a1c383b2077252c92f8394
Rickard Luu <rickardimine@gmail.com>
2022-11-22 17:15:00 +0100

    update

--- 11_buttons-that-do-stuff/03_stringer/script.js
+++ 11_buttons-that-do-stuff/03_stringer/script.js
@@ -1,17 +1,17 @@
 "use strict"; // We want to get some help from the strict mode to catch errors.
 
-/* 
- * 3. Stringer 
- * 
+/*
+ * 3. Stringer
+ *
  * Do this one in smaller groups or alone, uncommenting the code as you go.
- * 
+ *
  * TASK: You will need to:
  *   - Make a variable for the "add" button
  *   - Make variable for the paragraph where the strings shows up
  *   - Add an pointerdown event to the "add" button where you
  *     + Update the `stringerString`
  *     + Update the paragraph where the string shows up using `stringerString`
- * 
+ *
  *  Additionaly, if you have time, you should also
  *   - Add an onclick event for the "clear button", which,
  *     + Clears `stringerString` by setting it to the empty string
