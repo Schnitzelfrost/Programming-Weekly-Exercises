f017ff0664681172560c09953f02aa8ac77f2494
Rickard Luu <rickardimine@gmail.com>
2022-11-23 13:56:34 +0100

    taking desicions done

--- 21_taking-decisions/01_formValidation/script.js
+++ 21_taking-decisions/01_formValidation/script.js
@@ -6,42 +6,52 @@
  *
  */
 
+//setting up input form for username
 const inputUsername = document.querySelector("#form-validation-username");
+//setting up instruction for username
 const feedbackUsername = document.querySelector(
   "#form-validation-feedback-username"
 );
+//letting it be false for later use case
 let usernameCorrect = false;
 
+//setting up input form for password
 const inputPassword = document.querySelector("#form-validation-password");
+//setting up instruction for password
 const feedbackPassword = document.querySelector(
   "#form-validation-feedback-password"
 );
+//letting it be false for later use case
 let passwordCorrect = false;
 
+//setting up submit button
 const inputSubmit = document.querySelector("#form-validation-submit");
 
 // The username input
 function validateUsername(event) {
   const userInput = event.target.value; // This is how we get the thing the user changed.
 
+  //if the string is less than 5
   if (userInput.length < 5) {
+    //adding a new class, in this case its the class containing color red.
     inputUsername.classList.add("widget-input-error");
     feedbackUsername.classList.add("widget-input-error");
     usernameCorrect = false;
   } else {
     inputUsername.classList.remove("widget-input-error");
     feedbackUsername.classList.remove("widget-input-error");
+    // setting it to true when the statement is fulfilled
     usernameCorrect = true;
   }
 
+  //the reason for 2 of the exact same functions to exist in both pass and user, is for different use cases (orders)
   if (usernameCorrect && passwordCorrect) {
+    //self-explained element method
     inputSubmit.disabled = false;
   } else {
     inputSubmit.disabled = true;
   }
 }
-
-inputUsername.addEventListener("input", validateUsername);
 
 // The password input
 function validatePassword(event) {
@@ -65,4 +75,5 @@ function validatePassword(event) {
 }
 
 // The password input
+inputUsername.addEventListener("input", validateUsername);
 inputPassword.addEventListener("input", validatePassword);
