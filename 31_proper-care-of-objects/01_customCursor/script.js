509b39b15d9e61dd1759c5fc2275c08ed135b7a4
Rickard Luu <rickardimine@gmail.com>
2022-11-28 14:38:22 +0100

    update

--- 31_proper-care-of-objects/01_customCursor/script.js
+++ 31_proper-care-of-objects/01_customCursor/script.js
@@ -3,15 +3,15 @@
  *
  * TASK: Annotate each line with a comment, try to understand how everything
  *       connects. Look at the video for a reference code-thorugh.
- * 
+ *
  */
 
 let cursor;
 
 /**
- * Return an Element reprpesenting a custom cursor. 
- * @param {number} width 
- * @param {number} height 
+ * Return an Element reprpesenting a custom cursor.
+ * @param {number} width
+ * @param {number} height
  * @returns Element
  */
 function makeCustomCursor(width, height) {
@@ -28,39 +28,36 @@ function makeCustomCursor(width, height) {
 
   cursor.style.transition = "background-color 0.1s";
   cursor.style.backgroundColor = "black";
-  cursor.style.opacity = "30%";
+  cursor.style.opacity = "90%";
   cursor.style.border = "1px solid #00000011";
 
   return cursor;
 }
 
-
 /**
  * Draw a custom mouse under the mouse.
- * @param {PointerEvent} event 
+ * @param {PointerEvent} event
  */
 function updateCustomCursor(event) {
   const pointer = {
     x: event.clientX,
-    y: event.clientY
+    y: event.clientY,
   };
 
   // We need to subtract 50% to center the pointer
   cursor.style.transform = `translate(calc(${pointer.x}px - 50%), calc(${pointer.y}px - 50%))`;
 }
 
-
 /**
  * Make a cursor and add it to the DOM.
- * @param {PointerEvent} event 
+ * @param {PointerEvent} event
  */
 function addCustomCursor(event) {
   cursor = makeCustomCursor(30, 30);
   document.body.append(cursor);
 }
 
-
-// These 
+// These
 document.addEventListener("pointermove", addCustomCursor, { once: true });
 document.addEventListener("pointermove", updateCustomCursor);
 
@@ -72,19 +69,15 @@ document.addEventListener("pointerup", function (event) {
   cursor.style.opacity = "30%";
 });
 
-
 document.body.style.cursor = "none"; // comment this to show the original cursor
 
-
-
-
 /* CHALLENGE:
- * 
- * This is a good template to work from if you want to experiment with more advanced 
+ *
+ * This is a good template to work from if you want to experiment with more advanced
  * custom cursor interactions. A few of the things you could implement are:
  *
  *   - Change the cursor to an emoji.
  *   - Changing the shape/color of the cursor when you hover a specific DOM element.
  *   - Add a secondary shape that "follows" the cursor with a bit of a delay.
- * 
+ *
  */
